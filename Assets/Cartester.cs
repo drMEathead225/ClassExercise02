976c043ee8203c76b4ee9f5720fb3dbe24417340
drMEathead225 <hollow269@morrisville.edu>
2026-08-31 23:14:24 -0400

    gg

---
+++ Assets/Cartester.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+public class CarTester : MonoBehaviour
+{
+ // Start is called once before the first frame update
+ void Start()
+ {
+ // This is our test message!
+ Debug.Log("My awesome car is ready! Hello from ");
+ }
+}
